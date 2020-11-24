dcc2c4324eae2f82cfe813c488ed8083df7e8e63
edurocks <edu.vgama@gmail.com>
2020-11-24 15:28:33 +0000

    MVVM
    Small project to practice the MVVM pattern

--- app/src/main/java/dmanlancers/com/ui/recipes/RecipesFragment.kt
+++ app/src/main/java/dmanlancers/com/ui/recipes/RecipesFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import dagger.android.support.DaggerFragment
 import dmanlancers.com.R
 import dmanlancers.com.network.Resource
@@ -18,8 +19,6 @@ class RecipesFragment : DaggerFragment() {
     @Inject
     lateinit var providerFactory: ViewModelProviderFactory
     private lateinit var viewModel: RecipesViewModel
-   // private lateinit var ldClient : LDClient
-   // private lateinit var listenerFlag : FeatureFlagChangeListener
 
     companion object {
         fun newInstance() = RecipesFragment()
@@ -33,34 +32,13 @@ class RecipesFragment : DaggerFragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this, providerFactory).get(RecipesViewModel::class.java)
 
-
-      /*  val ldConfig = LDConfig.Builder()
-            .setMobileKey("mob-52a4c6d5-1b94-4ca3-8304-7267f4e19555")
-            .build()
-
-        val ldUser = LDUser.Builder("user_key")
-                    .email("example@gmail.com")
-                    .build()
-
-        ldClient = LDClient.init(activity?.application, ldConfig, ldUser, 5)
-
-        listenerFlag = FeatureFlagChangeListener { flagKey ->
-            val value = LDClient.get().boolVariation(flagKey, false)
-            if (!value){
-                recipes.setBackgroundColor(ContextCompat.getColor(context!!, R.color.colorAccent))
-            }else{
-                recipes.setBackgroundColor(ContextCompat.getColor(context!!, R.color.colorPrimary))
-            }
-        }*/
-
-       // LDClient.get().registerFeatureFlagListener("manage-mqtt-connection", listenerFlag)
-        viewModel.getMenus().observe(this, Observer {menus ->
+        viewModel.getMenus().observe(viewLifecycleOwner, Observer {menus ->
             if (menus != null){
                 when(menus.status){
 
                     Resource.Status.SUCCESS -> {
                         menus.data?.forEach {
-                            Log.e("nomes", it.name)
+                            Log.e("nomes", it.image)
                         }
                     }
 
@@ -72,11 +50,5 @@ class RecipesFragment : DaggerFragment() {
                 }
             }
         })
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-       // ldClient.close()
-       // ldClient.unregisterFeatureFlagListener("manage-mqtt-connection", listenerFlag)
     }
 }
